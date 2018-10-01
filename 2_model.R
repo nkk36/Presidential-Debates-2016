8b17771a0ce98edff23d35fba3c2470ef986b64c
nkk36 <nix847@hotmail.com>
2018-09-30 20:40:08 -0400

    started modeling with STMs

--- 2_model.R
+++ 2_model.R
@@ -1,9 +1,11 @@
 # Setup ====
 
 # Load packages
+library(dplyr)
 library(ldatuning)
 library(magrittr) 
 library(NLP)
+library(quanteda)
 library(slam)
 library(stm)
 library(stringi)
@@ -15,10 +17,13 @@ library(topicmodels)
 
 # Load functions
 source("R/custom_FindTopicsNumber_plot.R", chdir = T)
+source("R/reorder_within.R", chdir = T)
 
 # Load data ====
 
 load("data/3_cleaned_corpus.RData")
+
+df = read.csv("data/2_data_frame_documents.csv")
 
 
 # Convert to document term matrix ====
@@ -84,4 +89,69 @@ lemma_tm = lda %>%
 
 # STM ====
 
-stmdtm <- convert(dtm, to = "stm", docvars = docvars(corpus))
+t = tidy(corpus)
+
+processed = textProcessor(t$text, metadata = t)
+out = prepDocuments(processed$documents, processed$vocab, processed$meta)
+docs = out$documents
+vocab = out$vocab
+meta = out$meta
+
+stm_no_covariate = stm(documents = out$documents, 
+          vocab = out$vocab,
+          K = 20, 
+          prevalence =~ party,
+          max.em.its = 75, 
+          data = out$meta,
+          init.type = "Spectral")
+
+labelTopics(stm, c(6))
+
+plot(stm_no_covariate, type = "summary", xlim = c(0, .3), labeltype = "frex")
+
+
+td_beta <- tidy(stm_no_covariate)
+
+td_beta %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  mutate(topic = paste0("Topic ", topic),
+         term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(term, beta, fill = as.factor(topic))) +
+  geom_col(alpha = 0.8, show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free_y") +
+  coord_flip() +
+  scale_x_reordered() +
+  labs(x = NULL, y = expression(beta),
+       title = "Highest word probabilities for each topic",
+       subtitle = "Different words are associated with different topics")
+
+prep <- estimateEffect(1:20 ~ party, stm, meta = out$meta, uncertainty = "Global")
+
+stm = stm(documents = out$documents, 
+          vocab = out$vocab,
+          K = 20, 
+          prevalence =~ party,
+          max.em.its = 75, 
+          data = out$meta,
+          init.type = "Spectral",
+          content =~ party)
+
+plot(prep, 
+     covariate = "party", 
+     topics = seq(1,20,1),
+     model = stm, 
+     method = "difference",
+     cov.value1 = "Republican", 
+     cov.value2 = "Democrat",
+     xlab = "More Republican ... More Democrat",
+     main = "Effect of Liberal vs. Conservative",
+     xlim = c(-.5, .5), 
+     labeltype = "lift")
+
+
+
+
+
+
